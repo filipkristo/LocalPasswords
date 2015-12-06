deaa7dda0e6ff2f0c2f876d88f2bad728eb3f962
Filip Krišto <filipkristo@windowslive.com>
2015-12-06 18:33:34 +0100

    Added basic navigation template

--- LocalPasswords/LocalPasswords/Views/DrillInPage.xaml.cs
+++ LocalPasswords/LocalPasswords/Views/DrillInPage.xaml.cs
@@ -13,18 +13,26 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace LocalPasswords
+namespace LocalPasswords.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class DrillInPage : Page
     {
-        public MainPage()
+        public DrillInPage()
         {
             this.InitializeComponent();
+        }
+
+        private void ListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            this.Frame.Navigate(
+                typeof(BasicSubPage),
+                e.ClickedItem,
+                new Windows.UI.Xaml.Media.Animation.DrillInNavigationTransitionInfo());
         }
     }
 }
