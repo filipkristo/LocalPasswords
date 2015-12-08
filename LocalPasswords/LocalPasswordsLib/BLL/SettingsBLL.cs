8a74c3b70a9cfc752f643dfe7c073694ab7e3ab1
Filip Krišto <filipkristo@windowslive.com>
2015-12-08 16:05:49 +0100

    - Added language settings - Added viewmodels for all views

---
+++ LocalPasswords/LocalPasswordsLib/BLL/SettingsBLL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalPasswordsLib.BLL
+{
+    public class SettingsBLL
+    {
+        public SettingsBLL()
+        {
+
+        }
+
+        public void SaveLanguage(String Language)
+        {
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            localSettings.Values["Language"] = Language;
+        }
+
+        public String GetLanguage()
+        {
+            var localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            return localSettings.Values["Language"]?.ToString();
+        }
+    }
+}
